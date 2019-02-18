460493bcb666614d4b0d722946ebbd9f9f8b21b1
akshayd110 <akshayd110@gmail.com>
2019-02-18 17:30:24 +0530

    config file

---
+++ config.py
@@ -0,0 +1,16 @@
+#Db1
+DB1_CONFIG = {
+   'mongo_router_ip': '<ip_address_here>',
+    'mongo_port':  'port_here',
+    'mongo_username': 'username',
+    'mongo_password': 'password',
+}
+
+DB2_CONFIG = {
+   'mongo_router_ip': '<ip_address_here>',
+    'mongo_port':  'port_here',
+    'mongo_username': 'username',
+    'mongo_password': 'password',
+}
+
+#Db2
