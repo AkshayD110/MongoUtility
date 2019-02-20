83e8b1d4badf80938906455c0e939531d2250ffa
akshayd110 <akshayd110@gmail.com>
2019-02-21 00:56:41 +0530

    changes to connections implementation

--- mongoConnection.py
+++ mongoConnection.py
@@ -1,20 +1,48 @@
 import pymongo
 from pymongo import MongoClient
 import ssl
+import config
 
 class Connection:
-    def __init__(self,  mongoserver, port, username, password):
-        self.mongoserver = mongoserver
-        self.port = port
-        self.username = username
-        self.password = password
+    def __init__(self,  whichDb):
+        self.whichDb = whichDb
+
 
 
     def connectToMongo(self):
+        if (self.whichDb == 1):
+            mongoserver= config.PERF_LAB['mongo_router_ip']
+            port = config.PERF_LAB['mongo_port']
+            username = config.PERF_LAB['mongo_username']
+            password = config.PERF_LAB['mongo_password']
+            client = MongoClient(host=mongoserver, port=port, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+            client.the_database.authenticate(username, password, mechanism='SCRAM-SHA-1', source='admin')
+            return client
 
-        client = MongoClient(host=self.mongoserver, port=self.port, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
-        client.the_database.authenticate(self.username, self.password, mechanism='SCRAM-SHA-1', source='admin')
-        return client
+        elif (self.whichDb == 2):
+            mongoserver = config.PERF04_SOFTLAYER_LAB['mongo_router_ip']
+            port = config.PERF04_SOFTLAYER_LAB['mongo_port']
+            username = config.PERF04_SOFTLAYER_LAB['mongo_username']
+            password = config.PERF04_SOFTLAYER_LAB['mongo_password']
+            client = MongoClient(host=mongoserver, port=port, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+            client.the_database.authenticate(username, password, mechanism='SCRAM-SHA-1', source='admin')
+            return client
+
+        elif (self.whichDb == 3):
+            mongoserver = config.STORAGE_LAB['mongo_router_ip']
+            port = config.STORAGE_LAB['mongo_port']
+            username = config.STORAGE_LAB['mongo_username']
+            password = config.STORAGE_LAB['mongo_password']
+            client = MongoClient(host=mongoserver, port=port, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+            client.the_database.authenticate(username, password, mechanism='SCRAM-SHA-1', source='admin')
+            return client
+
+        else:
+            return "Enter one of the following values for selecting DB. " \
+                   "1" \
+                   "2" \
+                   "3"
+
         # db = client[self.tenant]
         # print("connected")
         # collection = db['compliance_audit_indexed']
